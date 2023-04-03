22b4822c3ee8c85df8b55091f622364d2c51add7
Abhishek Kumar <Abhishekkumar986874@gmail.com>
2023-04-03 14:58:11 +0530

    fix chat issue

--- src/Room/VideoChat.js
+++ src/Room/VideoChat.js
@@ -2,73 +2,102 @@
 import { Peer } from "peerjs";
 import React, { useEffect, useRef, useState } from "react";
 const VideoChat = ({ socket, roomid }) => {
-    const [mic, setMic] = useState(false);
-    const [peerId, setPeerId] = useState(null);
+    const [peerId, setPeerId] = useState('');
+    const [remotePeerIdValue, setRemotePeerIdValue] = useState('');
     const remoteVideoRef = useRef(null);
-    const localVideoRef = useRef(null);
+    const currentUserVideoRef = useRef(null);
     const peerInstance = useRef(null);
-
+    const [audio, setAudio] = useState(true);
+    const [video, setVideo] = useState(true);
+    const [screen, setScreen] = useState(false);
     useEffect(() => {
         const peer = new Peer();
-        peer.on("open", (id) => {
-            setPeerId(id);
+
+        peer.on('open', (id) => {
+            setPeerId(id)
         });
-        peer.on("call", (call) => {
+
+        peer.on('call', (call) => {
             var getUserMedia = navigator.getUserMedia;
-            getUserMedia({ video: false, audio: true }, (stream) => {
-                localVideoRef.current.srcObject = stream;
-                localVideoRef.current.play();
-                call.answer(stream);
-                call.on("stream", (remoteStream) => {
-                    remoteVideoRef.current.srcObject = remoteStream;
+
+            getUserMedia({ video: true, audio: true }, (mediaStream) => {
+                currentUserVideoRef.current.srcObject = mediaStream;
+                currentUserVideoRef.current.play();
+                call.answer(mediaStream)
+                call.on('stream', function (remoteStream) {
+                    remoteVideoRef.current.srcObject = remoteStream
                     remoteVideoRef.current.play();
                 });
             });
-        });
-        peerInstance.current = peer;
-
-        socket.on("Id", ({ peerId }) => {
-            connectToPeer(peerId);
         })
 
-        return () => {
-            peerInstance.current.destroy();
-            socket.off("Id");
-        };
+        socket.on('Id', (id) => {
+            setRemotePeerIdValue(id.peerId);
+        })
 
-    }, []);
+        peerInstance.current = peer;
+    }, [])
 
-    function join() {
-        socket.emit("Id", { roomid, peerId });
+
+    // mute video audio or stop video
+    const muteVideo = () => {
+        setVideo(!video);
     }
 
-    const connectToPeer = (remotePeerId) => {
+    const muteAudio = () => {
+        setAudio(!audio);
+    }
 
+    const stopVideo = () => {
+        setScreen(!screen);
+    }
+
+
+    useEffect(() => {
+        if (remotePeerIdValue) {
+            console.log('remotePeerIdValue', remotePeerIdValue)
+            console.log('peerId', peerId)
+            call(remotePeerIdValue);
+        }
+    }, [remotePeerIdValue])
+
+    const call = (remotePeerId) => {
         var getUserMedia = navigator.getUserMedia;
 
-        getUserMedia({ video: false, audio: true }, (stream) => {
+        getUserMedia({ video: true, audio: true }, (mediaStream) => {
 
-            localVideoRef.current.srcObject = stream;
-            localVideoRef.current.play();
+            currentUserVideoRef.current.srcObject = mediaStream;
+            currentUserVideoRef.current.play();
 
-            var call = peerInstance.current.call(remotePeerId, stream);
+            const call = peerInstance.current.call(remotePeerId, mediaStream)
 
-            call.on("stream", (remoteStream) => {
-                remoteVideoRef.current.srcObject = remoteStream;
+            call.on('stream', (remoteStream) => {
+                remoteVideoRef.current.srcObject = remoteStream
                 remoteVideoRef.current.play();
             });
         });
-    };
+        setScreen(true);
+    }
 
     return (
-        <div>
-            <div>
-                {
-                    peerId ? <button onClick={join}>Join Chat</button> : null
-                }
+        <div className="App">
+            <h1>Current user id is {peerId}</h1>
+            <button onClick={muteVideo}>Mute Video</button>
+            <button onClick={muteAudio}>Mute Audio</button>
+            <button onClick={stopVideo}>Stop Video</button>
+            {screen ? <> <div>
+                <video ref={currentUserVideoRef} muted />
             </div>
-            <audio ref={localVideoRef} autoPlay muted></audio>
-            <audio ref={remoteVideoRef} autoPlay></audio>
+                <div>
+                    <video ref={remoteVideoRef} />
+                </div></> : <button onClick={() => {
+                    socket.emit('Id', { roomid, peerId })
+                    setScreen(true);
+                }}>Call</button>
+            }
+
+
+
         </div>
     );
 };
